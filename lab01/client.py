892904bb36e2a9e9a26c9f7356d259879a44c3d7
a11411BCC034 <a11411BCC034@FACOM.(none)>
2017-04-05 12:11:34 -0300

    lab01 - progresso

---
+++ lab01/client.py
@@ -0,0 +1,10 @@
+import socket
+
+s = socket.socket()
+host = "localhost"
+port = 12345
+
+s.connect((host, port))
+print s.recv(1024)
+x = raw_input("")
+s.close()
---
+++ lab01/server.py
@@ -0,0 +1,15 @@
+import socket
+
+s = socket.socket()
+host = "localhost"
+port = 12345
+s.bind((host,port))
+
+s.listen(5)
+while True:
+	c, addr = s.accept()
+	print 'Got connection from', addr
+	c.send('Thank you for connecting')
+	print c.recv(1024)
+	x = raw_input("")
+	c.close()
